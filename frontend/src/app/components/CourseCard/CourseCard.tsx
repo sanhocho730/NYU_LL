ea35a0b98b265cd5b56b8f6c8aaca8b5169bb659
jh7316 <jh7316@nyu.edu>
2025-04-13 19:16:54 -0400

    CourseCard Layout

--- frontend/src/app/components/CourseCard/CourseCard.tsx
+++ frontend/src/app/components/CourseCard/CourseCard.tsx
@@ -1,6 +1,18 @@
+import styles from './CourseCard.module.scss'
+import Link from 'next/link'
+
 export default function CourseCard() {
-    return <div>
-        Course Card
-    </div>
+    return <Link href="/" className={styles.main}>
+        <h3>Course Name</h3>
+        <p>Prof. Professr Name</p>
+        <div className={styles.rating}>
+            <p>overall</p>
+            <h1>4.9</h1>
+        </div>
+        <div className={styles.difficulty}>
+            <p>difficulty</p>
+            <h1>1.9</h1>
+        </div>
+    </Link>
 
 }
