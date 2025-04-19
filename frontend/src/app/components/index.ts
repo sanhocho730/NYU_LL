083d9c8e369187782cf832fa2b46aa101f6a0ed0
jh7316 <jh7316@nyu.edu>
2025-04-19 18:10:16 -0400

    Add RatingCard Component

--- frontend/src/app/components/index.ts
+++ frontend/src/app/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from './Header/Header';
 export { default as Footer } from './Footer/Footer';
 export { default as CourseCard } from './CourseCard/CourseCard';
 export { default as SearchCourseCard } from "./SearchCourseCard/SearchCourseCard";
+export { default as RatingCard } from './RatingCard/RatingCard';
